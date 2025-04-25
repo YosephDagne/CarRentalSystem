a04c20f5101f7323252481417e933a413e11a339
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-04-25 08:04:13 +0300

    clear all data for redo

--- backend/routes/carRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllCars, getCarById } from "../controller/carController.js";
-
-const router = express.Router();
-
-router.get("/", getAllCars);
-router.get("/:id", getCarById);
-
-export default router;
